b7bb33af08850e2afb3cbde15fadc752d29bd8a8
Makhedonsky <121441697+makhedonsky@users.noreply.github.com>
2023-03-22 17:46:40 +0300

    Update aldiarSQL.py

--- aldiarSQL.py
+++ aldiarSQL.py
@@ -1,36 +1,38 @@
-import sqlite3 as sq
+import sqlite3
+from contextlib import closing
 
 
-#cursorM = connectionM.cursor()
-#cursorF = connectionF.cursor()
+
 
 ShoesCreateTable = "CREATE TABLE Shoes(category2 text, photo text, name text primary key, price text)"
 OuterwearCreateTable = "CREATE TABLE Outerwear(category2 text, photo text, name text primary key, price text)"
 PantsShoesCreateTable = "CREATE TABLE Pants(category2 text, photo text, name text primary key, price text)"
 AccessoriesCreateTable = "CREATE TABLE Accessories(category2 text, photo text, name text primary key, price text)"
 
-connectionM = sq.connect('MaleDB.db')
-connectionF = sq.connect('FemaleDB.db')
+
+
+connectionM = sqlite3.connect('MaleDB.db')
+connectionF = sqlite3.connect('FemaleDB.db')
 
 
 
 def sql_male(data):
-	connectionM = sq.connect('MaleDB.db')
+	connectionM = sqlite3.connect('MaleDB.db')
 
 	if data['category1'] == 'Аксессуары':
-		AccessoriesInsertTable = f"INSERT INTO Accessories(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		AccessoriesInsertTable = f"INSERT INTO Accessories(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionM.execute(AccessoriesInsertTable)
 
 	elif data['category1'] == 'Обувь':
-		ShoesInsertTable = f"INSERT INTO Shoes(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		ShoesInsertTable = f"INSERT INTO Shoes(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionM.execute(ShoesInsertTable)
 
 	elif data['category1'] == 'Верхняя одежда':
-		OuterwearInsertTable = f"INSERT INTO Outerwear(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		OuterwearInsertTable = f"INSERT INTO Outerwear(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionM.execute(OuterwearInsertTable)
 
 	elif data['category1'] == 'Штаны':
-		PantsInsertTable = f"INSERT INTO Pants(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		PantsInsertTable = f"INSERT INTO Pants(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionM.execute(PantsInsertTable)
 
 	connectionM.commit()
@@ -38,23 +40,124 @@ def sql_male(data):
 
 
 def sql_female(data):
-	connectionF = sq.connect('FemaleDB.db')
+	connectionF = sqlite3.connect('FemaleDB.db')
 
 	if data['category1'] == 'Аксессуары':
-		AccessoriesInsertTable = f"INSERT INTO Accessories(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		AccessoriesInsertTable = f"INSERT INTO Accessories(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionF.execute(AccessoriesInsertTable)
 
 	elif data['category1'] == 'Обувь':
-		ShoesInsertTable = f"INSERT INTO Shoes(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		ShoesInsertTable = f"INSERT INTO Shoes(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionF.execute(ShoesInsertTable)
 
 	elif data['category1'] == 'Верхняя одежда':
-		OuterwearInsertTable = f"INSERT INTO Outerwear(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		OuterwearInsertTable = f"INSERT INTO Outerwear(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionF.execute(OuterwearInsertTable)
 
 	elif data['category1'] == 'Штаны':
-		PantsInsertTable = f"INSERT INTO Pants(category2, photo, name, price) VALUES ('{data['category2'][7:]}', '{data['photo']}', '{data['name']}', '{data['price']}')"
+		PantsInsertTable = f"INSERT INTO Pants(category2, photo, name, price) VALUES ('{data['category2']}', '{data['photo']}', '{data['name']}', '{data['price']}')"
 		connectionF.execute(PantsInsertTable)
 
 	connectionF.commit()
 	connectionF.close()
+
+
+
+
+
+def sql_male_select(choise, product):
+	connectionM = sqlite3.connect('MaleDB.db')
+	cursorM = connectionM.cursor()
+	global SelectedList
+
+	if choise == 'Аксессуары':
+		AccessoriesSelect = f"SELECT * FROM Accessories WHERE category2 = '{product}'"
+
+		cursorM.execute(AccessoriesSelect)
+		SelectedList = cursorM.fetchall()
+
+
+	elif choise == 'Обувь':
+		ShoesSelect = f"SELECT * FROM Shoes WHERE category2 = '{product}'"
+
+		cursorM.execute(ShoesSelect)
+		SelectedList = cursorM.fetchall()
+
+
+	elif choise == 'Верхняя одежда':
+		OuterwearSelect = f"SELECT * FROM Outerwear WHERE category2 = '{product}'"
+
+		cursorM.execute(OuterwearSelect)
+		SelectedList = cursorM.fetchall()
+
+
+	elif choise == 'Штаны':
+		PantsSelect = f"SELECT * FROM Pants WHERE category2 = '{product}'"
+
+		cursorM.execute(PantsSelect)
+		SelectedList = cursorM.fetchall()
+
+	return SelectedList
+
+
+
+def sql_female_select(choise, product):
+	connectionF = sqlite3.connect('FemaleDB.db')
+	cursorF = connectionF.cursor()
+	global SelectedList
+
+	if choise == 'Аксессуары':
+		AccessoriesSelect = f"SELECT * FROM Accessories WHERE category2 = '{product}'"
+
+		cursorF.execute(AccessoriesSelect)
+		SelectedList = cursorM.fetchall()
+
+
+	elif choise == 'Обувь':
+		ShoesSelect = f"SELECT * FROM Shoes WHERE category2 = '{product}'"
+
+		cursorF.execute(ShoesSelect)
+		SelectedList = cursorF.fetchall()
+
+
+	elif choise == 'Верхняя одежда':
+		OuterwearSelect = f"SELECT * FROM Outerwear WHERE category2 = '{product}'"
+
+		cursorF.execute(OuterwearSelect)
+		SelectedList = cursorF.fetchall()
+
+
+	elif choise == 'Штаны':
+		PantsSelect = f"SELECT * FROM Pants WHERE category2 = '{product}'"
+
+		cursorF.execute(PantsSelect)
+		SelectedList = cursorF.fetchall()
+
+	return SelectedList
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
